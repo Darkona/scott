a74df5b204f8f19d9ac74b3ede6dde9f3ab6319b
Javier Darkona <javier.darkona@gmail.com>
2025-08-27 23:20:40 -0400

    using marker filter faster logs

--- src/main/java/com/darkona/scott/DemoService.java
+++ src/main/java/com/darkona/scott/DemoService.java
@@ -5,6 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 public class DemoService {
 
@@ -32,23 +34,35 @@ public class DemoService {
         return "-> salute2" + salute3();
     }
 
+
     @Logged
     public String salute() {
         return "O-> salute1" + salute2();
     }
+
     @Logged
     private String salute3() {
         return "-> salute 3" + salute4();
     }
 
+    @Logged
     private String salute4(){
         return "-> Salute 4: Hello";
     }
+
     @Logged(redactArgValues = {"denominator"})
     public double divide(double numerator, double denominator) {
         if (denominator == 0) {
             throw new IllegalArgumentException("Can't divide by zero");
         }
         return numerator / denominator;
+    }
+
+    //This log should only be sent to the LOKI appender (see logback-spring.xml and application-local.yaml)
+    // So we add the marker "LOKI"
+    @Logged(marker = "LOKI")
+    public void IAmInvisible(Map<String, String> attr) {
+        log.info("There wasn't any automatic log from DemoService::IAmInvisible before this one in the console by logged. But you should be able to see it in Grafana");
+        log.info(attr.toString());
     }
 }
--- src/main/java/com/darkona/scott/StartupActions.java
+++ src/main/java/com/darkona/scott/StartupActions.java
@@ -9,6 +9,8 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+
 @Component
 
 public class StartupActions {
@@ -18,25 +20,39 @@ public class StartupActions {
     private static final Logger log = LoggerFactory.getLogger(StartupActions.class);
     private final LogDecorator deco;
 
-    public StartupActions(DemoService demoService, LogDecorator deco) {this.demoService = demoService;
+    public StartupActions(DemoService demoService, LogDecorator deco) {
+        this.demoService = demoService;
         this.deco = deco;
     }
 
     @EventListener(ApplicationReadyEvent.class)
     @WithSpan("MySpan")
     @Logged
-    public void onReady(){
+    public void onReady() {
 
         log.info("MDC MAP = {}", org.slf4j.MDC.getCopyOfContextMap());
         log.warn(deco.red("------- Scott Starting --------"));
         log.info(deco.pink(demoService.salute()));
-        demoService.sum(2,3);
+        demoService.sum(2, 3);
         demoService.divide(4500, 25);
         try {
             demoService.explode();
-        }catch(Exception e){
+        } catch (Exception e) {
             // ignore
         }
-        log.info(deco.green("------- Scott Started --------"));
+
+        demoService.IAmInvisible(Map.of(
+                "1", deco.green("Bulbasaur"),
+                "2", deco.green("Ivysaur"),
+                "3", deco.green("Venusaur"),
+                "4", deco.cyan("Squirtle"),
+                "5", deco.cyan("Wartortle"),
+                "6", deco.cyan("Blastoise"),
+                "7", deco.orange("Charmander"),
+                "8", deco.orange("Charmeleon"),
+                "9", deco.orange("Charizard")
+
+        ));
+        log.info(deco.green("------- Scott Finished Startup Actions --------"));
     }
 }
